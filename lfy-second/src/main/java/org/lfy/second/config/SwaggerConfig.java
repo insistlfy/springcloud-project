198d0929aa474bc14a864cda9798112f79b2356c
LFY <lifuyuan@swifthealth.cn>
2021-03-18 11:00:27 +0800

    add Eureka

---
+++ lfy-second/src/main/java/org/lfy/second/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package org.lfy.second.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * SwaggerConfig
+ * ① ： @EnableSwagger3 ，该注解开启swagger支持
+ *
+ * @author lfy
+ * @date 19-3-21
+ **/
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("基于Swagger3.0.0的接口文档-【EUREKA SERVER】")
+                .description("LFY SECOND API")
+                .termsOfServiceUrl("https://www.baidu.com/")
+                .version("version ２.0")
+                .build();
+    }
+}
