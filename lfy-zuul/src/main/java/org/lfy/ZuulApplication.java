137778650097c7aa24e15a8c4ca9dba0ecdb7b31
LFY <lifuyuan@swifthealth.cn>
2021-03-20 11:41:07 +0800

    add Zuul Gateway

--- lfy-zuul/src/main/java/org/lfy/ZuulApplication.java
+++ lfy-zuul/src/main/java/org/lfy/ZuulApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * ZuulApplication
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @date 2021/3/18
  **/
 @Slf4j
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class ZuulApplication {
