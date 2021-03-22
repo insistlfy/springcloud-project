dcf04d8a7193044e0cf8c6eadc9df55bdb8d1137
LFY <lifuyuan@swifthealth.cn>
2021-03-22 15:22:23 +0800

    add Springboot admin

---
+++ lfy-admin/README.md
@@ -0,0 +1,3 @@
+## lfy-admin
+* #### [spring Boot Admin 官网文档](https://codecentric.github.io/spring-boot-admin/current/)
+
--- lfy-admin/src/main/java/org/lfy/admin/AdminApplication.java
+++ lfy-admin/src/main/java/org/lfy/admin/AdminApplication.java
@@ -1,5 +1,6 @@
 package org.lfy.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,11 +8,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * AdminApplication
+ * ① ： @EnableAdminServer，开启监控
  *
  * @author lfy
  * @date 2021/3/20
  **/
 @Slf4j
+@EnableAdminServer
 @EnableEurekaClient
 @SpringBootApplication
 public class AdminApplication {
