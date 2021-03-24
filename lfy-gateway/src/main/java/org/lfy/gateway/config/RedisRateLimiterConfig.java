bf20ba23a9a1f45f09bcf3630f7b9580ef49b6d2
LFY <lifuyuan@swifthealth.cn>
2021-03-24 18:06:08 +0800

    add RequestLimitRater

---
+++ lfy-gateway/src/main/java/org/lfy/gateway/config/RedisRateLimiterConfig.java
@@ -0,0 +1,32 @@
+package org.lfy.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Mono;
+
+/**
+ * RedisRateLimiterConfig
+ * 限流策略的配置类
+ * ① userKeyResolver ： 根据请求参数中的username进行限流
+ * ② ipKeyResolver ： 根据访问IP进行限流
+ *
+ * @author lfy
+ * @date 2021/3/24
+ **/
+@Configuration
+public class RedisRateLimiterConfig {
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("username"));
+    }
+
+    @Bean
+    @Primary
+    public KeyResolver ipKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+
+}
